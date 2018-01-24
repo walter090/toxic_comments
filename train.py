4f4dea4387d480683a682ff26c6127b849a41359
walter_wu090 <walter.wu090@gmail.com>
2018-01-23 22:01:19 -0500

    Call properties before initialization

--- train.py
+++ train.py
@@ -9,54 +9,59 @@ from toxic_detection import ToxicityCNN
 def train(csvs, batch_size, num_epochs,
           vocab_size, embedding_size, num_labels,
           verbose_freq=2000, save=True, restore=False,
-          meta=None):
-    model = ToxicityCNN(csvs=csvs, batch_size=batch_size, num_epochs=num_epochs,
-                        vocab_size=vocab_size, embedding_size=embedding_size, num_labels=num_labels)
+          meta=None, comment_length=60):
 
-    init_op = tf.global_variables_initializer()
-    saver = tf.train.Saver(var_list=tf.all_variables())
-    sess = tf.Session()
-    timestamp = datetime.datetime.now().isoformat('_')
+    model = ToxicityCNN(csvs=csvs, batch_size=batch_size,
+                        num_epochs=num_epochs, vocab_size=vocab_size,
+                        embedding_size=embedding_size, num_labels=num_labels,
+                        comment_length=comment_length)
 
     all_summaries = tf.summary.merge_all()
-
+    timestamp = datetime.datetime.now().isoformat('_')
     save_dir = os.path.abspath(os.path.join(os.path.curdir, 'models_and_visual', timestamp))
     log_dir = os.path.join(save_dir, 'tensorboard')
     model_dir = os.path.join(save_dir, 'saved_models')
 
-    writer = tf.summary.FileWriter(log_dir, sess.graph)
+    model_optimization = model.optimize
+    model_step = model.global_step
+    model_loss = model.loss
 
-    if not restore:
-        sess.run(tf.local_variables_initializer())
-        sess.run(tf.global_variables_initializer())
-    else:
-        restore_variables(meta, sess)
+    init_op = tf.group(tf.global_variables_initializer(),
+                       tf.local_variables_initializer())
 
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
+    with tf.Session() as sess:
+        writer = tf.summary.FileWriter(log_dir, sess.graph)
 
-    try:
-        while not coord.should_stop():
-            sess.run(init_op)
-            _, step, loss, summaries = sess.run([model.optimize, model.global_step,
-                                                 model.loss, all_summaries])
-            writer.add_summary(summaries, global_step=step)
-            cur_time = datetime.datetime.now().isoformat('-')
-            if step % verbose_freq == 0:
-                print('{} - At step {}, loss {}'.format(cur_time, step, loss))
+        sess.run(init_op)
 
-    except tf.errors.OutOfRangeError:
-        print('Done training -- epoch limit reached')
-    finally:
-        coord.request_stop()
+        if restore:
+            restore_variables(meta, sess)
 
-    coord.join(threads)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
 
-    if save:
-        cur_step = tf.train.global_step(sess, model.global_step)
-        saver.save(sess, save_path=model_dir, global_step=cur_step)
+        try:
+            while not coord.should_stop():
+                _, step, loss, summaries = sess.run([model_optimization, model_step,
+                                                     model_loss, all_summaries])
 
-    sess.close()
+                writer.add_summary(summaries, global_step=step)
+
+                cur_time = datetime.datetime.now().isoformat('-')
+                if step % verbose_freq == 0:
+                    print('{} - At step {}, loss {}'.format(cur_time, step, loss))
+
+        except tf.errors.OutOfRangeError:
+            print('Done training -- epoch limit reached')
+        finally:
+            coord.request_stop()
+
+        coord.join(threads)
+
+        if save:
+            saver = tf.train.Saver(var_list=tf.global_variables())
+            cur_step = tf.train.global_step(sess, model.global_step)
+            saver.save(sess, save_path=model_dir, global_step=cur_step)
 
 
 def restore_variables(meta, sess):
