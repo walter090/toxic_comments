0e7412fd4d797005e462be6955f870f6db451cfa
walter_wu090 <walter.wu090@gmail.com>
2018-02-05 04:15:46 -0500

    Return auc score and op

--- model/toxic_detection.py
+++ model/toxic_detection.py
@@ -190,9 +190,9 @@ class ToxicityCNN(Model):
 
     @property_wrap('_metric')
     def metric(self):
-        auc, _ = tf.metrics.auc(labels=self.toxicity_batch,
-                                predictions=self._prediction)
-        self._metric = tf.reduce_mean(auc)
+        auc, auc_op = tf.metrics.auc(labels=self.toxicity_batch,
+                                     predictions=self._prediction)
+        self._metric = tf.reduce_mean(auc), auc_op
         return self._metric
 
     @property_wrap('_optimize')
