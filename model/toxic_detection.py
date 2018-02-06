f4209615ac78e34325bf46812d7bc5022a9eb515
walter_wu090 <walter.wu090@gmail.com>
2018-02-06 02:33:43 -0500

    Remove special input for testing

--- model/toxic_detection.py
+++ model/toxic_detection.py
@@ -33,12 +33,13 @@ class ToxicityCNN(Model):
             self.file_read_op(csvs, batch_size, num_epochs,
                               num_labels, comment_length)
 
-    def file_read_op(self, file_names, batch_size,
+    def file_read_op(self, file_names, validation_files, batch_size,
                      num_epochs, num_labels=None, comment_length=None):
         """Read csv files in batch
 
         Args:
             file_names: list, list of file names.
+            validation_files: list, list of validation file names.
             batch_size: int, batch size.
             num_epochs: int, number of epochs.
             num_labels: int, number of labels.
@@ -57,32 +58,19 @@ class ToxicityCNN(Model):
 
         _, value = reader.read(queue)
 
-        if not self.testing:
-            record_defaults = [[''], [''], [0], [0], [0], [0], [0], [0], *([[-3]] * 60)]
-            cols = tf.decode_csv(value, record_defaults=record_defaults)
-            comment_id = cols[0]
-            comment_text = tf.stack(cols[-60:])
-            toxicity = tf.stack(cols[2:8])
+        record_defaults = [[''], [''], [0], [0], [0], [0], [0], [0], *([[-3]] * 60)]
+        cols = tf.decode_csv(value, record_defaults=record_defaults)
+        comment_id = cols[0]
+        comment_text = tf.stack(cols[-60:])
+        toxicity = tf.stack(cols[2:8])
 
-            min_after_dequeue = 10000
-            capacity = min_after_dequeue + 4 * batch_size
-            self.comment_batch, self.toxicity_batch, self.id_batch = tf.train.shuffle_batch(
-                [comment_text, toxicity, comment_id], batch_size=batch_size,
-                capacity=capacity, min_after_dequeue=min_after_dequeue
-            )
-            self.toxicity_batch = tf.cast(self.toxicity_batch, dtype=tf.float32)
-        else:
-            record_defaults = [[''], [''], *([[-3]] * 60)]
-            cols = tf.decode_csv(value, record_defaults=record_defaults)
-            comment_id = cols[0]
-            comment_text = tf.stack(cols[-60:])
-
-            min_after_dequeue = 10000
-            capacity = min_after_dequeue + 4 * batch_size
-            self.comment_batch, self.id_batch = tf.train.shuffle_batch(
-                [comment_text, comment_id], batch_size=batch_size,
-                capacity=capacity, min_after_dequeue=min_after_dequeue
-            )
+        min_after_dequeue = 10000
+        capacity = min_after_dequeue + 4 * batch_size
+        self.comment_batch, self.toxicity_batch, self.id_batch = tf.train.shuffle_batch(
+            [comment_text, toxicity, comment_id], batch_size=batch_size,
+            capacity=capacity, min_after_dequeue=min_after_dequeue
+        )
+        self.toxicity_batch = tf.cast(self.toxicity_batch, dtype=tf.float32)
 
     def network(self, x_input=None, num_output=None,
                 layer_config=None, fully_conn_config=None, pool='max',
