334a1db619c96cb0eeb62284b8e0d8208f083131
walter_wu090 <walter.wu090@gmail.com>
2018-03-13 01:36:54 -0400

    Remove unnecessary tensor reshape

--- model/rnn.py
+++ model/rnn.py
@@ -137,9 +137,8 @@ class ToxicityLSTM(Model):
             last_hidden = tf.gather(outputs, last_indices)
 
             if self.attention:
-                outputs_in_batch = tf.reshape(outputs, shape=[batch_size, -1, state_size * 2 if bi else state_size])
-                attention_weights = structure.weigh_attention(outputs_in_batch)
-                last_hidden = structure.get_context_vector(source_hidden=outputs_in_batch,
+                attention_weights = structure.weigh_attention(outputs)
+                last_hidden = structure.get_context_vector(source_hidden=outputs,
                                                            attention_weights=attention_weights)
 
             logits = tf.matmul(last_hidden, weights)
