daf78f58cb5e7dde1373096c65912751c47cf860
walter_wu090 <walter.wu090@gmail.com>
2018-03-22 02:04:10 -0400

    Preprocess input text

--- training/client.py
+++ training/client.py
@@ -1,5 +1,8 @@
+import string
 import time
 from argparse import ArgumentParser
+
+import nltk
 
 from grpc.beta import implementations
 from tensorflow.contrib.util import make_tensor_proto
@@ -27,36 +30,51 @@ def parse_args():
 
     host, port = args.server.split(':')
 
-    return host, port, args.text
+    return host, port, args.text, args.word2id
 
 
 def main():
-    host, port, text = parse_args()
+    host, port, text, word2id = parse_args()
 
     # Process string
-    # TODO Translate string to numeric
+    # Translate string to numeric
+    punctuations = list(string.punctuation)
+    punctuations += ['``', "''"]
+
+    word_list = nltk.word_tokenize(text)
+
+    word_list = [word.lower() for word in word_list if word not in punctuations]
+
+    data = []
+
+    for word in word_list:
+        try:
+            translated_word = word2id[word]
+        except KeyError:
+            translated_word = word2id['<unk>']
+        data.append(translated_word)
+
+    if len(data) < 40:
+        data += [word2id['<pad>']] * (40 - len(data))
 
     channel = implementations.insecure_channel(host, int(port))
     stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
 
-    with open(text, 'rb') as f:
-        data = f.read()
+    start = time.time()
 
-        start = time.time()
+    request = predict_pb2.PredictRequest()
 
-        request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'cls'
+    request.model_spec.signature_name = 'predict_text'
+    request.inputs['text'].CopyFrom(make_tensor_proto(data))
 
-        request.model_spec.name = 'cls'
-        request.model_spec.signature_name = 'predict_text'
-        request.inputs['text'].CopyFrom(make_tensor_proto(data, shape=[1]))
+    result = stub.Predict(request, 60.0)
 
-        result = stub.Predict(request, 60.0)
+    end = time.time()
+    time_diff = end - start
 
-        end = time.time()
-        time_diff = end - start
-
-        print(result)
-        print('Time elapsed: {}'.format(time_diff))
+    print(result)
+    print('Time elapsed: {}'.format(time_diff))
 
 
 if __name__ == '__main__':
